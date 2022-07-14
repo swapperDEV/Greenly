8e3fd9e003e32e8527fdddb34d187e53a55ac4f2
Wiktor Maciązek <75098062+swapperDEV@users.noreply.github.com>
2022-07-14 16:24:56 +0200

    0.1

--- views/home/HomeView.tsx
+++ views/home/HomeView.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 import styles from "./home.module.scss";
+import flower from "../../assets/flower.png";
+import cactus from "../../assets/cactus.png";
+import Image from "next/image";
 export const HomeView = () => {
-  return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.homeDescription}>
+        <p className={styles.headTitle}>
+          Can you help <br />
+          us
+          <a className={styles.headTitleGreen}> Greenly</a>
+          <a className={styles.headTitleLightGreen}>.</a>
+          <br /> a world?{" "}
+          <div className={styles.flower}>
+            <Image src={flower} />
+          </div>
+        </p>
+      </div>
+      <div className={styles.image}>
+        <div>
+          <Image src={cactus} />
+        </div>
+      </div>
+      <div className={styles.pros}></div>
+    </div>
+  );
 };
